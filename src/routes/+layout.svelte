535dd06ba79f73c19f63127c559ee239557eb5b9
ddochea0314 <ddochea0314@gmail.com>
2023-03-01 18:10:48 +0900

    tag 표기 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,34 +8,44 @@
 	<title>Test Page</title>
 </svelte:head>
 <div class="drawer">
-	<input id="my-drawer" type="checkbox" class="drawer-toggle" bind:checked={isOpen} />
-	<div class="drawer-content">
-	  <!-- Page content here -->
-	  <div class="navbar bg-base-100">
-		<div class="flex-none">
-		  <button class="btn btn-square btn-ghost" on:click={() => { isOpen = !isOpen }}>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-		  </button>
+	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col">
+		<!-- Navbar -->
+		<div class="w-full navbar">
+			<div class="flex-none lg:hidden">
+				<label for="my-drawer-3" class="btn btn-square btn-ghost">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						class="inline-block w-6 h-6 stroke-current"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						/></svg
+					>
+				</label>
+			</div>
+			<div class="flex-1 px-2 mx-2">
+				<a href="/">Home</a>
+			</div>
+			<div class="flex-none hidden lg:block">
+				<ul class="menu menu-horizontal">
+					<!-- Navbar menu content here -->
+				</ul>
+			</div>
 		</div>
-		<div class="flex-1">
-			<a class="btn btn-ghost normal-case text-xl" href="/">Home</a>
+		<!-- Page content here -->
+		<div class="container mx-auto">
+			<slot />
 		</div>
-		<!-- <div class="flex-none">
-		  <button class="btn btn-square btn-ghost">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-		  </button>
-		</div> -->
-	  </div>
-	  <div class="container mx-auto">
-		<slot />
-	  </div>
-	</div> 
-	<div class="drawer-side">
-	  <label for="my-drawer" class="drawer-overlay"></label> 
-	  <ul class="menu p-4 w-80 bg-base-100 text-base-content">
-		<!-- Sidebar content here -->
-		<li><a>Sidebar Item 1</a></li>
-		<li><a>Sidebar Item 2</a></li>
-	  </ul>
 	</div>
-  </div>
+	<div class="drawer-side">
+		<label for="my-drawer-3" class="drawer-overlay" />
+		<ul class="menu p-4 w-80 bg-base-100">
+			<!-- Sidebar content here -->
+		</ul>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,22 @@
 
 	export let data: { posts: Post[] }; // +page.ts load 함수 결과값
 </script>
+
 <ul>
-	{#each data.posts as item}
+	{#each data.posts as post}
 		<li class="card m-4 bg-primary text-primary-content">
 			<div class="card-body">
-				<h2 class="card-title">{item.title}</h2>
-				<p>{item.date}</p>
-				<p>{item.tag}</p>
+				<h2 class="card-title">{post.title}</h2>
+				<p>{post.date}</p>
+				<p>
+					{#each post.tag as tag}
+						<span class="badge badge-warning gap-2">{tag}</span>
+					{/each}
+				</p>
 				<div class="card-actions justify-end">
-				  <a class="btn" href="{item.path}">더보기</a>
+					<a class="btn" href={post.path}>더보기</a>
 				</div>
-			  </div>
+			</div>
 		</li>
 	{/each}
 </ul>
