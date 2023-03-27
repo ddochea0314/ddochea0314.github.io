69196a1f2c491a26f54e513d2ec0c2d753fbdf8b
ddochea0314 <ddochea0314@gmail.com>
2023-03-27 21:02:11 +0900

    태그에서 페이지 경로 이동시 tag/post/{slug} 형식이 되는 버그 수정

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,12 +12,12 @@
 		<article class="card bg-base-300 h-96">
 			<div class="card-body">
 				<h2 class="card-title text-md">
-					<a href={post.path} class="truncate">{post.title}</a>
+					<a href={`/${post.path}`} class="truncate">{post.title}</a>
 				</h2>
 				<span class="text-sm">{post.date}</span>
 				<p class="card-subtitle">
 					<span class="text-md line-clamp-4 text-ellipsis overflow-hidden">
-						<a href={post.path}>{post.description ?? ''}</a>
+						<a href={`/${post.path}`}>{post.description ?? ''}</a>
 					</span>
 				</p>
 				<div class="card-actions">
--- src/routes/tags/[tag]/+page.ts
+++ src/routes/tags/[tag]/+page.ts
@@ -2,7 +2,6 @@
 import type { Load } from '@sveltejs/kit';
 
 export const load: Load = async ({ fetch, params }) => {
-    console.log(params)
 	const res = await fetch('/api/tags/' + params.tag);
 	const posts = await res.json();
 	return {
