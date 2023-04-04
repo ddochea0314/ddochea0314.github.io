738eb7fd02ffb52b1bf2a5099ea23bbacd5f1ebe
ddochea0314 <ddochea0314@gmail.com>
2023-04-04 23:22:13 +0900

    post 가져오기 lib 통합 및 sitemap 구현

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,15 +1,70 @@
 interface Post {
+	/**
+	 * 포스트 제목입니다.
+	 */
 	title: string;
+	/**
+	 * 포스트 작성일입니다.
+	 * ISO 8601 형식을 사용합니다.
+	 */
 	date: string;
+	/**
+	 * 포스트 업데이트 일시입니다.
+	 * ISO 8601 형식을 사용합니다.
+	 */
+	updated: string | null;
+	/**
+	 * 포스트 요약 내용입니다.
+	 */
 	description: string;
+	/**
+	 * 포스트 태그 배열입니다.
+	 */
 	tags: Array<string>;
+	/**
+	 * 포스트 URL 경로입니다.
+	 */
 	path: string;
 }
 
-enum NavbarType {
-	Home = 'Home',
-	Post = 'Post'
+// enum NavbarType {
+// 	/**
+// 	 * 홈 링크를 나타냅니다.
+// 	 */
+// 	Home = 'Home',
+// 	/**
+// 	 * 포스트 링크를 나타냅니다.
+// 	 */
+// 	Post = 'Post'
+// }
+
+
+/**
+ * 모든 블로그 포스트 메타데이터를 포함하는 객체 배열을 반환합니다.
+ * @param {function} [filter] 포스트 조건에 대한 필터 배열함수입니다. 생략하면 모든 요소가 포함됩니다.
+ * @param {object} [thisArg] 콜백 함수를 실행할 때 this로 사용할 객체입니다.
+ * @returns {Promise<Post[]>} 모든 블로그 포스트의 메타데이터를 포함하는 객체 배열입니다.
+ */
+async function getPosts(filter?: (value: Post, index: number, array: Post[]) => unknown, thisArg?: any) : Promise<Post[]> {
+	const allPostFiles = import.meta.glob<Post>('/src/routes/posts/docs/*.md'); // vite 지원기능
+	const iterables = Object.entries(allPostFiles);
+
+	return Promise.all<Post>(
+		iterables.map(async ([path, resolver]: any) => {
+			const { metadata } = await resolver();
+			const postPath = path.replace('/src/routes/posts/docs/', 'posts/').replace('.md', '');
+			return {
+				...metadata,
+				path: postPath
+			};
+		})
+	)
+	.then((posts) => filter ? posts.filter(filter, thisArg) : posts)
+	.then((posts) => posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()));
 }
 
 export type { Post };
-export { NavbarType };
+export { 
+	// NavbarType,
+	getPosts,
+};
---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,59 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import { getPosts } from '$lib';
+
+export const GET: RequestHandler = async ({setHeaders}) => {
+    setHeaders({
+        'Content-Type': 'application/xml',
+        'Cache-Control': 'max-age=0, s-max-age=3600'
+    });
+
+    const website = 'https://ddochea0314.github.io';
+
+    const posts = (await getPosts());
+    const tags = posts.map(p => p.tags).flat().filter((v, i, a) => a.indexOf(v) === i);
+    
+    const xml = `<?xml version="1.0" encoding="UTF-8" ?>
+    <urlset
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+      xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"
+      xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+      xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
+      xmlns:pagemap="http://www.google.com/schemas/sitemap-pagemap/1.0"
+      xmlns:xhtml="http://www.w3.org/1999/xhtml"
+    >
+      <url>
+        <loc>${website}</loc>
+        <priority>1.0</priority>
+      </url>
+      ${posts
+        .map(
+          (post) => `<url>
+            <loc>${website}/${post.path}</loc>
+            <lastmod
+              >${
+                post.updated
+                  ? new Date(post.updated).toISOString()
+                  : new Date(post.date).toISOString()
+              }</lastmod
+            >
+            <changefreq>monthly</changefreq>
+            <priority>1.0</priority>
+          </url>`
+        )
+        .join('')
+    }
+    ${
+        tags.map(
+            (tag) => `<url>
+                <loc>${website}/tags/${tag}</loc>
+                <changefreq>monthly</changefreq>
+                <priority>0.8</priority>
+            </url>`
+        ).join('')
+    }
+    </urlset>`
+    return new Response(xml);
+};
