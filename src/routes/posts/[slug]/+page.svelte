7ee1c5c6982dd5a12b7a94314df99f55832d175c
ddochea0314 <ddochea0314@gmail.com>
2023-04-07 00:25:42 +0900

    포스트 글자색 변경

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -17,7 +17,7 @@
 <Nav />
 <main class="container">
 	<section class="bg-base-100 rounded-box p-4 md:m-16 m-1">
-		<article class="prose mx-auto pt-12 pb-12">
+		<article class="prose dark:text-slate-400 mx-auto pt-12 pb-12">
 			<svelte:component this={data.content} />
 			<span>Published: {data.date}</span>
 		</article>
